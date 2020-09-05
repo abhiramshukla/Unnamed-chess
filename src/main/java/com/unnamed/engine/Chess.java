78973716ad615c7d6d26db09e67457d5db44038a
Abhiram Shukla <shukla.abhiram12@gmail.com>
2020-09-05 21:34:54 +0530

    Created a chess class to house the main function

---
+++ src/main/java/com/unnamed/engine/Chess.java
@@ -0,0 +1,11 @@
+package com.unnamed.engine;
+
+import com.unnamed.engine.Board.Board;
+
+public class Chess {
+
+    public static void main(String[] args) {
+        Board board = Board.createStandardBoard();
+        System.out.println(board);
+    }
+}
