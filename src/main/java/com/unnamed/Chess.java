a2e6ec699ecacedbfe30e610cd90f055266f4fd1
Abhiram Shukla <shukla.abhiram12@gmail.com>
2020-09-07 13:53:58 +0530

    Minor updates

--- src/main/java/com/unnamed/Chess.java
+++ src/main/java/com/unnamed/Chess.java
@@ -1,11 +1,13 @@
-package com.unnamed.engine;
+package com.unnamed;
 
 import com.unnamed.engine.Board.Board;
+import com.unnamed.gui.Table;
 
 public class Chess {
 
     public static void main(String[] args) {
         Board board = Board.createStandardBoard();
         System.out.println(board);
+        Table table = new Table();
     }
 }
